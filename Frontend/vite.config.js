e0bb4862a3474c58a0f3c803c934916bddcf1de8
Rizwan Fahim <rizwanfahim011@outlook.com>
2024-12-25 19:48:35 +0500

    Add files via upload

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    rollupOptions: {
+      // Use external only if you want to exclude it from the bundle.
+      external: [],
+    },
+  },
+});
